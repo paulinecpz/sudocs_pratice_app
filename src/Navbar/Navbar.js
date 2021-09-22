83a4ff083131fe27b9ccbbfdf0d6e0b80a8683ef
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-23 00:13:31 +0200

    working on the dropdown

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import logo from '../logo.svg';
 import styled from 'styled-components'
 import { NavLink as Link } from 'react-router-dom'
