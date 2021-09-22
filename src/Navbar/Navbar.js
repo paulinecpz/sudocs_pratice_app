820bbcc3b8433bcdb3f0e976a60ffdb8dbf9ef8c
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-22 18:42:28 +0200

    added react router for menu links

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -3,26 +3,31 @@ import logo from '../logo.svg';
 
 const Navbar = () => {
     return (
-        <Nav>
-            <NavLink to="/">
-                <img src={logo} className="nav-logo" alt="logo" />
-            </NavLink>
-            <Bars />
-            <NavMenu>
-                <NavLink tp="/features" activeStyle>
-                    Features
+        <>
+            <Nav>
+                <NavLink to="/">
+                    <img src={logo} className="nav-logo" alt="logo" />
                 </NavLink>
-                <NavLink tp="/pricing" activeStyle>
-                    Pricing
-                </NavLink>
-                <NavLink tp="/about" activeStyle>
-                    About
-                </NavLink>
-                <NavLink tp="/login" activeStyle>
-                    Log in
-                </NavLink>
-            </NavMenu>
-        </Nav>
+                <Bars />
+                <NavMenu>
+                    <NavLink tp="/features" activeStyle>
+                        Features
+                    </NavLink>
+                    <NavLink tp="/pricing" activeStyle>
+                        Pricing
+                    </NavLink>
+                    <NavLink tp="/about" activeStyle>
+                        About
+                    </NavLink>
+                    <NavLink tp="/login" activeStyle>
+                        Sign Up
+                    </NavLink>
+                </NavMenu>
+                <BavBtn>
+                    <NavBtnLink to="/login">Login</NavBtnLink>
+                </BavBtn>
+            </Nav>
+        </>
     )
 }
 
