6151d9585ad7d8d3010cc4158efd3d1377599aba
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-24 18:23:01 +0200

    added menu drawer and working on structure

--- src/Navbar/MuiNav.js
+++ src/Navbar/MuiNav.js
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import logo from '../logo.svg';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import { FaBars } from 'react-icons/fa';
 import MenuIcon from '@mui/icons-material/Menu';
+import MenuDrawer from './MenuDrawer'
 
 const MUInav = () => {
   return (
-    <Box sx={{ flexGrow: 1,}}>
+    <Box sx={{ flexGrow: 1, }}>
       <AppBar position="static" color="transparent" className="app-bar">
         <Toolbar className="nav-links">
-          <Button color="inherit" className="nav-logo"><img src={logo}  alt="logo" /></Button>
+          <Button color="inherit" className="nav-logo"><img src={logo} alt="logo" /></Button>
           <Button color="inherit" className="menu-link">Features</Button>
           <Button color="inherit" className="menu-link">Pricing</Button>
           <Button color="inherit" className="menu-link">About</Button>
           <Button color="inherit" className="menu-link">Login</Button>
           <IconButton
-          
+
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            {/* <FaBars className="icon-bars" /> */}
             <MenuIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
+      <MenuDrawer />
     </Box>
   );
 }
