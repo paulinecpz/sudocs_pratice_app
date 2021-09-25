38df0630f0380f499d8a5560361e41a092311a52
Kathy Tavia <kathy.tavia@gmail.com>
2021-09-25 16:49:11 +0200

    added link pages and  imported to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar/Navbar';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// pages
+import Features from './Pages/Features';
+import Pricing from './Pages/Pricing';
+import About from './Pages/About';
+import Login from './Pages/Login';
+
 function App() {
   return (
     <div className="App">
